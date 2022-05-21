71cfc49968f3edfc5492b7105cec354c107d3986
yangjianxiang <tospery@gmail.com>
2022-05-22 02:39:13 +0800

    text

--- lib/routes/text.dart
+++ lib/routes/text.dart
@@ -46,6 +46,47 @@ class _TextRouteState extends State<TextRoute> {
             "Hello world " * 6, //字符串重复六次
             textAlign: TextAlign.center,
           ),
+          Text(
+            "Hello world",
+            style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18.0,
+                height: 1.2,
+                fontFamily: "Courier",
+                background: Paint()..color = Colors.yellow,
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.dashed),
+          ),
+          Text.rich(
+            TextSpan(children: [
+              const TextSpan(text: "Home: "),
+              TextSpan(
+                  text: "https://flutterchina.club",
+                  style: const TextStyle(color: Colors.blue),
+                  recognizer: _tapRecognizer),
+            ]),
+          ),
+          DefaultTextStyle(
+            //1.设置文本默认样式
+            style: const TextStyle(
+              color: Colors.red,
+              fontSize: 20.0,
+            ),
+            textAlign: TextAlign.start,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                Text("hello world"),
+                Text("I am Jack"),
+                Text(
+                  "I am Jack",
+                  style: TextStyle(
+                      inherit: false, //2.不继承默认样式
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
